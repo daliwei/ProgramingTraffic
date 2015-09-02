4cc06331f9e6861e93364f2117f78ce214331fa1
Dali Wei_PATH_PC <trafficdali@gmail.com>
2015-09-01 19:13:52 -0700

    Initial Submission

---
+++ ProgramingTraffic/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProgramingTraffic.MvcApplication" Language="C#" %>
---
+++ ProgramingTraffic/Models/ListViewModel.cs
@@ -0,0 +1,22 @@
+﻿using ProgramingTraffic.Core;
+using ProgramingTraffic.Core.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProgramingTraffic.Models
+{
+    public class ListViewModel
+    {
+        public IList<Post> Posts { get; set; }
+
+        public int TotalPosts { get; set; }
+
+        public ListViewModel(IBlogRepository _blogRepository, int p)
+        {
+            Posts = _blogRepository.Posts(p - 1, 10);
+            TotalPosts = _blogRepository.TotalPosts();
+        }
+    }
+}
